d78fe0d850621e42c3d1d619f87e054abcb55473
sahoryherrera <sahory0920@gmail.com>
2020-10-23 14:48:49 -0600

    Clase Modificada 1

--- Lab2_SahoryCano/src/lab2_sahorycano/Detectives.java
+++ Lab2_SahoryCano/src/lab2_sahorycano/Detectives.java
@@ -15,7 +15,60 @@ public class Detectives {
     public Detectives() {
     } // Fin Constructor Granos Basicos
     
+    public Detectives(String nombre, int edad, String nacionalidad, int añosLab, int nivel) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.añosLab = añosLab;
+        this.nivel = nivel;
+    } // Fin Constructor Granos Basicos
     
+    //mutadores
+     public String getNombre() {
+        return nombre;
+    } // Fin Get Nombre
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    } // Fin Set Nombre
+
+    public int getedad() {
+        return edad;
+    } // Fin Get edad
+
+    public void setedad(int edad) {
+        this.edad = edad;
+    } // Fin Set edad
+    
+     public String getnacionalidad() {
+        return nacionalidad;
+    } // Fin Get Nacionalidad
+
+    public void setnacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    } // Fin Set Nacionalidad
+    
+     public int getañosLab() {
+        return añosLab;
+    } // Fin Get años laborales
+
+    public void setañosLab(int añosLab) {
+        this.añosLab = añosLab;
+    } // Fin Set años laborales
+    
+      public int getnivel() {
+        return nivel;
+    } // Fin Get edad
+
+    public void setnivel(int nivel) {
+        this.nivel = nivel;
+    } // Fin Set edad
+    
+    //MA
+    public String toString() {
+        return "[Nombre: " + nombre + "][Edad: " + edad + "][Nacionalidad: " + nacionalidad + "][Años Laborales: " + añosLab + "][Nivel: " + nivel + ']';
+    } // Fin To String
+
 
 }//fin
 
